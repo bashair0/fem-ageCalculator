bf58c7e672993db5cbe8bde2a211da32afc3ed2f
bashayer <bisho5.ba@gmail.com>
2023-11-27 09:02:45 +0800

    Add checkMonth and checkMonthValidation functions

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ const submitBtn = document.querySelector('.submit-btn')
 const rootStyles = getComputedStyle(document.documentElement)
 const primaryRed = rootStyles.getPropertyValue('--clr-primary-red')
 
+function checkYearValidation (birthYear) {
+  const currentYear = new Date().getFullYear()
+  return birthYear <= currentYear ? true : 'Must be in the past'
+}
+
+function checkMonthValidation (birthMonth) {
+  return birthMonth < 13 && birthMonth > 0 ? true : false
+}
+
+function checkMonth (birthMonth) {
+  if (!checkMonthValidation(birthMonth)) {
+    return 'Must be a valid month'
+  } else {
+    let days = 31
+    if (birthMonth === 2) {
+      days = 28
+    } else if (
+      birthMonth === 4 ||
+      birthMonth === 6 ||
+      birthMonth === 9 ||
+      birthMonth === 11
+    ) {
+      days = 30
+    }
+    return days
+  }
+}
+
+console.log(checkMonth(1))
+
 function checkEmptyInput () {
   inputField.forEach(i => {
     if (i.value === '') {
