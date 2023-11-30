0cb15f730ffc09aa2f211afc3def3c5f315ba213
bashayer <bisho5.ba@gmail.com>
2023-11-30 09:31:12 +0800

    Add calculateAge function and add some major changes over all functions

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const errorMsg = document.querySelector('.err-msg')
-const label = document.querySelectorAll('label')
-const inputField = document.querySelectorAll('input[type="number"]')
+/* create a dateInput variable and get the day input */
+const dateInput = document.querySelector('#day')
+/* create a monthInput variable and get the month input */
+const monthInput = document.querySelector('#month')
+/* create a yearInput variable and get the year input */
+const yearInput = document.querySelector('#year')
+/* create an inputs variable and get all inputs pf type number*/
+const inputFields = document.querySelectorAll('input[type="number"]')
+/* create labels variable and get all labels */
+const labels = document.querySelectorAll('label')
+/* create submit button variable and get the submit button */
 const submitBtn = document.querySelector('.submit-btn')
-
+/* get custom property color values */
 const rootStyles = getComputedStyle(document.documentElement)
 const primaryRed = rootStyles.getPropertyValue('--clr-primary-red')
+const lightGrey = rootStyles.getPropertyValue('--clr-neutral-lightGrey')
+/* create a variable currentYear and get the current year value*/
+const currentYear = new Date().getFullYear()
+/* create a variable currentMonth and get the current month value */
+const currentMonth = new Date().getMonth() + 1
+/* create a variable currentMonth and get the current date value */
+const currentDate = new Date().getDate()
+/* create a birthDate variable and store the dateInput value in it */
+let birthMonth = monthInput.value
+let birthYear = yearInput.value
 
-function checkYearValidation (birthYear) {
-  const currentYear = new Date().getFullYear()
-  return birthYear <= currentYear ? true : false /* 'Must be in the past' */
+/* check if any of the inputs are empty */
+/* create a variable emptyInput and check if it was empty return false otherwise return true */
+function checkEmptyInput (emptyInput) {
+  return emptyInput.trim() === '' ? false : true
+  /* trim() method just accepts string type values */
 }
 
+/* check if user's year input is valid or not
+if birthYear is greater than currentYear print out "Must be in the past" */
+function checkYearValidation () {
+  const yearErrorMsg = document.querySelector('#year-validation')
+  let birthYear = yearInput.value
+  yearErrorMsg.textContent = ''
+  if (!checkEmptyInput(birthYear)) {
+    yearErrorMsg.textContent = 'This field is required'
+  } else {
+    if (birthYear > currentYear) {
+      yearErrorMsg.textContent = 'Must be in the past'
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+/* check if month 2 has 28 or 29 days */
 function adjustLeapYear (year) {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return 29
@@ -19,17 +58,31 @@ function adjustLeapYear (year) {
   }
 }
 
-function checkMonthValidation (birthMonth) {
-  return birthMonth < 13 && birthMonth > 0 ? true : false
+function checkMonthValidation () {
+  let birthMonth = monthInput.value
+  const monthErrorMsg = document.querySelector('#month-validation')
+  monthErrorMsg.textContent = ''
+  if (!checkEmptyInput(birthMonth)) {
+    monthErrorMsg.textContent = 'This field is required'
+  } else {
+    if (Number(birthMonth) < 1 || Number(birthMonth) > 12) {
+      monthErrorMsg.textContent = 'Must be a valid month'
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
-function checkMonth (birthMonth) {
+function checkMonth () {
+  let birthMonth = Number(monthInput.value)
   if (!checkMonthValidation(birthMonth)) {
     return false /* 'Must be a valid month' */
   } else {
     let days = 31
+    let birthYear = Number(yearInput.value)
     if (birthMonth === 2) {
-      days = adjustLeapYear(2012)
+      days = adjustLeapYear(birthYear)
     } else if (
       birthMonth === 4 ||
       birthMonth === 6 ||
@@ -42,19 +95,70 @@ function checkMonth (birthMonth) {
   }
 }
 
-function checkDateValidation (date) {
-  return date < 1 || date > 31 ? false : true
+function checkDateValidation () {
+  let birthDate = dateInput.value
+  let days = checkMonth()
+  const dateErrorMsg = document.querySelector('#date-validation')
+  dateErrorMsg.textContent = ''
+  if (!checkEmptyInput(birthDate)) {
+    dateErrorMsg.textContent = 'This field is required'
+  } else {
+    if (Number(birthDate) < 1 || Number(birthDate) > 31) {
+      dateErrorMsg.textContent = 'Must be a valid day'
+      return false
+    } else {
+      if (Number(birthDate) > days) {
+        dateErrorMsg.textContent = 'Must be a valid date'
+        return false
+      }
+      return true
+    }
+  }
 }
 
-function checkEmptyInput () {
-  inputField.forEach(i => {
-    if (i.value === '') {
-      label.forEach(l => {
+function checkIsString (inputValue) {
+  return isNaN(inputValue) ? false : true
+}
+
+function calculateAge () {
+  const yearAge = document.querySelector('.year-age')
+  const monthAge = document.querySelector('.month-age')
+  const dayAge = document.querySelector('.day-age')
+
+  let birthDate = Number(dateInput.value)
+  let birthMonth = Number(monthInput.value)
+  let birthYear = Number(yearInput.value)
+
+  yearAge.textContent = currentYear - birthYear
+  monthAge.textContent = currentMonth - birthMonth
+  dayAge.textContent = currentDate - birthDate
+}
+
+function checkValidation (whatever) {
+  inputFields.forEach(i => {
+    labels.forEach(l => {
+      l.style.color = lightGrey
+      i.style.borderColor = lightGrey
+    })
+  })
+  if (!whatever) {
+    inputFields.forEach(i => {
+      labels.forEach(l => {
         l.style.color = primaryRed
         i.style.borderColor = primaryRed
       })
-    }
-  })
+    })
+    return false
+  }
+  return true
 }
 
-submitBtn.addEventListener('click', checkEmptyInput)
+submitBtn.addEventListener('click', () => {
+  if (
+    checkValidation(checkDateValidation()) &&
+    checkValidation(checkMonthValidation()) &&
+    checkValidation(checkYearValidation())
+  ) {
+    calculateAge()
+  }
+})
